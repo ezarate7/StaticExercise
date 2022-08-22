e75e25160b8b53c97315b8b404d6b621f7179515
Eddie Z <e.zarate7@hotmail.com>
2022-08-22 10:08:32 -0600

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using StaticExercise;
+
+
+var celsius = TempConverter.FahrenheitToCelsius(0);
+var fahrenheit = TempConverter.CelsiusToFahrenheit(10);
+
+Console.WriteLine($"celcius {celsius}");
+Console.WriteLine($"fahrenheit {fahrenheit}");
